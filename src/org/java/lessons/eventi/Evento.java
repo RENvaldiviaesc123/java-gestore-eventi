2d453af58a124ed97c3cae950e5cb06bf12c7a45
renat <guitarra123primera@gmail.com>
2023-09-05 11:04:55 +0200

    Project Setup (First two milestones).

---
+++ src/org/java/lessons/eventi/Evento.java
@@ -0,0 +1,13 @@
+package org.java.lessons.eventi;
+
+public class Evento {
+    //ATTRIBUTI
+
+
+    //COSTRUTTORI
+
+    //GETTER E SETTER
+
+
+    //METODI
+}
---
+++ src/org/java/lessons/eventi/Main.java
@@ -0,0 +1,24 @@
+package org.java.lessons.eventi;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
